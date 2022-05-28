874cf7b3c65adde0a56012e702549952bb9675ed
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:11:24 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs13.js
@@ -0,0 +1,9 @@
+function tugas13() {
+    var buah = ["Pisang","Jeruk"];
+    console.log(buah);
+    buah.unshift("Mangga");
+    console.log(buah);
+    buah.unshift("Apel");
+    console.log(buah);
+}
+tugas13();
